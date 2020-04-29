aadc2614e75f93c1e7cfa7c4360fdc3975a8ee0b
Einar Lennelöv <lennelov@kth.se>
2020-04-29 22:10:28 +0200

    add dataset generation

---
+++ data_utils/preprocess.py
@@ -0,0 +1,41 @@
+import numpy as np
+import tensorflow as tf
+
+
+def extract_videos_and_labels(dict_dataset, n_classes=None):
+    """Take dict dataset (train, test, or valid) and return lists of tensors and labels."""
+    videos = []
+    labels = []
+    for video_id in dict_dataset:
+        videos.append(dict_dataset[video_id]['data'])
+        labels.append(dict_dataset[video_id]['action_label'])
+    label_array = make_one_hot_encoding(labels, n_classes=n_classes)
+    label_tensor = tf.convert_to_tensor(label_array)
+    return videos, label_tensor
+
+
+def extract_frames_and_labels(dict_dataset, n_classes=None):
+    """Take dict dataset (train, test, or valid) and return lists of tensors and labels."""
+    videos = []
+    labels = []
+    for video_id in dict_dataset:
+        videos.append(dict_dataset[video_id]['data'])
+        labels.append(dict_dataset[video_id]['action_label'])
+    frames = tf.concat(videos, 0)
+
+    label_array = make_one_hot_encoding(labels)
+    label_tensor = tf.convert_to_tensor(label_array)
+    return frames, label_tensor
+
+
+def make_one_hot_encoding(labels, n_classes=None):
+    """Take labels encoded as ints and return one-hot encoded array."""
+    n_labels = len(labels)
+    if n_classes is None:
+        n_classes = max(labels)+1
+
+    label_array = np.zeros((n_labels, n_classes))
+    for i, label in enumerate(labels):
+        label_array[i, label] = 1
+
+    return label_array
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from data_utils.data_loader import load_data
+from data_utils import preprocess
+import tensorflow as tf
+
+N_CLASSES = 174
 
 data, label_dict = load_data("./data/something-something-mini")
+
+videos, labels = preprocess.extract_videos_and_labels(data['train'], n_classes=N_CLASSES)
+frames, labels = preprocess.extract_frames_and_labels(data['train'], n_classes=N_CLASSES)
+# videos_dataset = tf.data.Dataset.from_tensors(videos) # currently broken
+frames_dataset = tf.data.Dataset.from_tensor_slices(frames)
+print(frames_dataset)
