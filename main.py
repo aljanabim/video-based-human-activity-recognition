b08169ce0ade1849cadef741969d2857e71deba3
Einar Lennelöv <lennelov@kth.se>
2020-05-03 14:02:44 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from data_utils.data_loader import load_data
+from data_utils import data_loader
 from data_utils import preprocess
+from data_utils import records
 import tensorflow as tf
 
 N_CLASSES = 174
 
 # Load data from disk
-data, label_dict = load_data("./data/something-something-mini")
+data, label_dict = data_loader.load_data("./data/something-something-mini")
 
 # Extract data and labels
 videos, labels = preprocess.extract_videos_and_labels(data['train'], n_classes=N_CLASSES)
@@ -26,47 +27,7 @@ videos_dataset = tf.data.Dataset.from_generator(
     output_shapes=((None, frame_height, frame_width, frame_n_channels), (n_classes,)))
 frames_dataset = tf.data.Dataset.from_tensor_slices((frames, labels))
 
-print(videos_dataset)
-print(frames_dataset)
-
-
 # Make tfrecords
-
-def _tensor_to_bytes_feature(value):
-    if tf.executing_eagerly():
-        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.numpy().tostring()]))
-    else:
-        print(value)
-        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.numpy.tostring()]))
-
-
-def _int_feature(value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-def _serialize_frame_sample(frame, label):
-    # print(frame)
-    feature = {'frame': _tensor_to_bytes_feature(frame),
-               'label': _tensor_to_bytes_feature(label)}
-    return tf.train.Example(features=tf.train.Features(feature=feature)).SerializeToString()
-
-
-def tf_serialize_frame_sample(frame, label):
-    tf_string = tf.py_function(
-        _serialize_frame_sample,
-        (frame, label),
-        tf.string)
-    return tf.reshape(tf_string, ())
-
-
-for frame, label in frames_dataset.take(1):
-    # print(frame.numpy())
-    _serialize_frame_sample(frame, label)
-
-print("Starting serialization")
-serialized_frames_dataset = frames_dataset.map(tf_serialize_frame_sample)
-print("Serialization complete")
-record_file_path = "test.tfrecord"
-writer = tf.data.experimental.TFRecordWriter(record_file_path)
-writer.write(serialized_frames_dataset)
-print("TFRecord created")
+records.serialize_dataset(dataset=frames_dataset,
+                          output_directory='../test_records',
+                          name="test-1")
