649d069b9046a8f4a7fe94c50252db53d09cd12f
Lennelov <einarlennelov@gmail.com@git config --global user.email einarlennelov@gmail.comgit config --global user.email einarlennelov@gmail.com@>
2020-05-05 10:45:23 +0200

    Add subfolder option

--- config.py
+++ config.py
@@ -6,17 +6,20 @@ JSON_PATH = './data/20bn-something-something-v2-jason'
 RECORDS_PATH = './data/records/'
 class Config():
     def __init__(self, root_path=ROOT_PATH, json_path=JSON_PATH, n_classes=174,
-                 img_width=455, img_height=256, max_frames=70, n_frames=40):
+                 img_width=455, img_height=256, max_frames=70, n_frames=40, use_subfolders=False):
+
+        separator = '/' if use_subfolders else '-'
+
         self.root_path = os.path.expanduser(root_path)
 
         # videos to be used
-        self.videos_path = os.path.expanduser("{}-video".format(root_path))
+        self.videos_path = os.path.expanduser("{}{}video".format(root_path, separator))
         # annonations of the used videos
         self.jason_label_path = json_path
 
         ## output folders
-        self.label_path =   os.path.expanduser("{}-label".format(root_path))
-        self.frame_path =  os.path.expanduser("{}-frame".format(root_path))
+        self.label_path = os.path.expanduser("{}{}label".format(root_path, separator))
+        self.frame_path = os.path.expanduser("{}{}frame".format(root_path, separator))
 
         self.n_classes = n_classes
         self.img_width = img_width
