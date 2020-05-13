05c0cfc0dbded64d2c27a43b0524cd9c809150b3
Jotix16 <zhobromikel@gmail.com>
2020-05-13 16:41:02 +0200

    works with video input and camera

--- demo/live_demo.py
+++ demo/live_demo.py
@@ -1,4 +1,9 @@
-#
+""" USAGE
+python3 live_demo.py -o here.avi -m ../models/trained_models/LSTM_50epochs_trimmed/ \
+    -i ../data/kth-actions/video/handwaving/person03_handwaving_d1_uncomp.avi
+"""
+
+
 from tensorflow.keras.models import load_model
 from collections import deque
 import numpy as np
@@ -6,87 +11,99 @@ import argparse
 import pickle
 import cv2
 import time
+
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", required=True,
 	help="path to trained serialized model")
-ap.add_argument("-i", "--input", required=True,
+ap.add_argument("-i", "--input", default=None,
 	help="path to our input video")
-ap.add_argument("-o", "--output", required=True,
+ap.add_argument("-o", "--output", default=None,
 	help="path to our output video")
-ap.add_argument("-s", "--size", type=int, default=128,
+ap.add_argument("-s", "--size", type=int, default=6,
 	help="size of queue for averaging")
 ap.add_argument("-bs", "--buffer_size", type=int, default=16,
 	help="size of queue for averaging")
 
-args = vars(ap.parse_args())
-# load the trained modelfrom disk
-print("[INFO] loading model ...")
-model = load_model(args["model"])
-lb = ["first","second", "third","mustafa", "einar","mikel"]
 
-# initialize the image mean for mean subtraction along with the
-# predictions queue
-Buffer = deque(maxlen=args["buffer_size"])
-Q = deque(maxlen=args["size"])
+def main():
+    args = vars(ap.parse_args())
 
+    print("[INFO] loading model ...")
+    model = load_model(args["model"])
+    lb = ["boxing","handclapping", "handwaving","jogging", "running","walking"]
 
-# initialize the video stream(input video or camera)
-vs = cv2.VideoCapture(args["input"])
-# vs = cv2.VideoCapture(0) #<--- camera
-writer = None #pointer to output video file
-(W, H) = (None, None)
-# loop over frames from the video file stream
-while True:
-    time.sleep(0.1)
-	# read the next frame from the file
-    (grabbed, frame) = vs.read()
-	# if the frame was not grabbed, then we have reached the end
-	# of the stream
-    if not grabbed:
-        break
-
-    if (W is None) or (H is None):
-        (H, W) = frame.shape[:2]
-        print(H,W)
-    
-    #TODO: frame to blacknwhite if necesary
-    #TODO: keep deque with frames
-    # frame to be augmented for output
-    model_input = cv2.resize(frame,(160,160))
-    model_input = cv2.cvtColor(model_input, cv2.COLOR_BGR2GRAY)[:,:,np.newaxis]
-
-    Buffer.append(cv2.resize(model_input,(160,160)))
-    output = frame.copy()
-
-    # preds = model.predict(np.expand_dims(frame, axis=0))[0]
-    preds = np.random.rand(1,6)
-    Q.append(preds)
-    # perform prediction averaging over the current history of
-    # previous predictions
-    results = np.array(Q).mean(axis=0)
-    i = np.argmax(results)
-    label = lb[i]
-    print(label)
-    
-    # draw the activity on the output frame
-    text = f"activity: {label}"
-    cv2.putText(output, text, (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-	# check if the video writer is None
-    if writer is None:
-        # initialize our video writer
-        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-        writer = cv2.VideoWriter(args["output"], fourcc, 30,
-            (W, H), True)
-    # write the output frame to disk
-    writer.write(output)
-	# show the output image
-    cv2.imshow("Output", output)
-    key = cv2.waitKey(1) & 0xFF
-	
-    if key == ord("q"): # break if the `q` key is pressed-*
-        break
-    
-    # break
+    Buffer = deque(maxlen=args["buffer_size"]) #frame buffer
+    Q = deque(maxlen=args["size"]) #prediction buffer
 
 
+    # initialize the video stream(input video or camera)
+    if args["input"] is not None:
+        vs = cv2.VideoCapture(args["input"])
+    else:
+        vs = cv2.VideoCapture(0) #<--- camera
+
+    label = ""
+
+
+    writer = None #pointer to output video file
+    (W, H) = (None, None)
+
+    counter =0
+    while True:
+
+        counter =np.mod(counter+1,1999)
+
+        (grabbed, frame) = vs.read()
+        # if the frame was not grabbed, then we have reached the end
+        # of the stream
+        if not grabbed:
+            break
+
+        if (W is None) or (H is None):
+            (H, W) = frame.shape[:2]
+            print(H,W)
+        
+        # frame to be augmented for output
+        model_input = cv2.resize(frame,(160,160))
+        model_input = cv2.cvtColor(model_input, cv2.COLOR_BGR2GRAY)[:,:,np.newaxis]
+        Buffer.append(cv2.resize(model_input,(160,160)))
+
+
+        output = frame.copy()
+        if len(Buffer)==16 and np.mod(counter, 5):
+            preds = model(np.repeat(np.expand_dims(np.array(Buffer), axis=[0,4]), repeats=3, axis=4  )).numpy()
+            Q.append(preds)
+            # perform prediction averaging 
+            results = np.array(Q).mean(axis=0)
+            i = np.argmax(results)
+            label = lb[i]
+            print(label)
+        
+        # draw the activity on the output frame
+        text = 'activity: {}'.format(label)
+        cv2.putText(output, text, (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+        # show the output image
+        cv2.imshow("Output", output)
+        key = cv2.waitKey(1) & 0xFF
+        
+
+        # write the output frames to disk
+        if writer is None and args["output"] is not None:
+            # initialize our video writer
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = cv2.VideoWriter(args["output"], fourcc, 30,
+                (W, H), True)
+        else:
+            time.sleep(0.1)
+        writer.write(output)
+
+        # break if the `q` key is pressed-*
+        if key == ord("q"): 
+            break
+
+
+if __name__ == "__main__":
+    main()
