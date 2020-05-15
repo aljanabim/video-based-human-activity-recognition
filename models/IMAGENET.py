afe497684c84d9a4054bc7e1a15311932f7f36c5
aljanabim <ma970101@yahoo.se>
2020-05-15 12:03:38 +0200

    formatting

--- models/IMAGENET.py
+++ models/IMAGENET.py
@@ -5,71 +5,83 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.regularizers import l2
 
-def Imagenet(input_shape=(160,160,3), name ='inception', weights=False, trainable=False, include_top=False):
+
+def Imagenet(input_shape=(160, 160, 3), name='inception', weights=False, trainable=False, include_top=False):
     # Create the base model pre-trained on imagenet
     # the pretrained models come with input_shape = (160, 160, 3)
     # include_top=False removes the classification layer on top.
     # returned model returns a feature vector of dimension D from a given image
-    if name=='mobilnet':
+    if name == 'mobilnet':
         # D = 1280
         base_model = tf.keras.applications.MobileNet(
-                                                input_shape=input_shape,
-                                                include_top=include_top,
-                                                weights='imagenet')
-    elif name=='inception':
+            input_shape=input_shape,
+            include_top=include_top,
+            weights='imagenet')
+    elif name == 'inception':
         # D = 2048
         base_model = tf.keras.applications.InceptionV3(
-                                                input_shape=input_shape,
-                                                include_top=include_top,
-                                                weights='imagenet')
-    elif name=='vgg':
+            input_shape=input_shape,
+            include_top=include_top,
+            weights='imagenet')
+    elif name == 'vgg':
         # D = 2048
         base_model = tf.keras.applications.VGG16(
-                                                input_shape=input_shape,
-                                                include_top=include_top,
-                                                weights='imagenet')
-    elif name=='resnet':
+            input_shape=input_shape,
+            include_top=include_top,
+            weights='imagenet')
+    elif name == 'resnet':
         # D = 2048
         base_model = tf.keras.applications.ResNet50(
-                                                input_shape=input_shape,
-                                                include_top=include_top,
-                                                weights='imagenet')
-    else: raise ValueError(name+" is not a valid model name. Give mobilnet or inception")
+            input_shape=input_shape,
+            include_top=include_top,
+            weights='imagenet')
+    elif name == 'resnet_v2':
+        # D = 2048
+        base_model = tf.keras.applications.ResNet50V2(
+            input_shape=input_shape,
+            include_top=include_top,
+            weights='imagenet')
+    else:
+        raise ValueError(
+            name + " is not a valid model name. Give mobilnet or inception")
 
-    base_model.trainable=trainable
+    base_model.trainable = trainable
     return base_model
+
 
 def Video_Feature_Extractor(base_model):
     # returns an [BATCH x FRAME_NR x D] tensor where D is the dimension of the features
     feature_extractor = tf.keras.Sequential([
         base_model,
         GlobalAveragePooling2D()
-        ])
+    ])
 
     features = TimeDistributed(
-        layer = feature_extractor,
-        input_shape = base_model.layers[0].input_shape[0]
-        )
+        layer=feature_extractor,
+        input_shape=base_model.layers[0].input_shape[0]
+    )
 
-    return  tf.keras.Sequential([features])
+    return tf.keras.Sequential([features])
+
 
 def LSTM_Video_Classifier(features, class_nr, optimizer='adam'):
     # model
     full_model = tf.keras.Sequential([
         features,
-        LSTM(128, input_shape=(None,256)),
+        LSTM(128, input_shape=(None, 256)),
         Dense(512, kernel_initializer="he_normal"),
         BatchNormalization(),
         Dense(class_nr)
-        ])
-    
-    #compile model
+    ])
+
+    # compile model
     full_model.compile(
         optimizer=optimizer,
         loss=CategoricalCrossentropy(from_logits=True),
         metrics=['accuracy']
-        )
+    )
     return full_model
+
 
 def AVG_Video_Classifier(features, class_nr, optimizer='adam'):
     # model
@@ -78,15 +90,14 @@ def AVG_Video_Classifier(features, class_nr, optimizer='adam'):
         GlobalAveragePooling1D(),
         Dense(2048, kernel_initializer="he_normal"),
         Dense(class_nr, kernel_initializer="he_normal")])
-    
-    #compile model
+
+    # compile model
     full_model.compile(
         optimizer=optimizer,
         loss=CategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'])
 
     return full_model
-
 
 
 # Doc:
@@ -115,12 +126,12 @@ if __name__ == "__main__":
     # featuer_ex.summary()
 
     # 3)Get the Video Classifiers (add a classifying model on top which can trained on given data)
-    lstm_video_classifier = LSTM_Video_Classifier(features=featuer_ex3, class_nr=6, optimizer=RMSprop(lr=0.0001))
+    lstm_video_classifier = LSTM_Video_Classifier(
+        features=featuer_ex3, class_nr=6, optimizer=RMSprop(lr=0.0001))
     lstm_video_classifier.summary()
 
     # avg_video_classifier = AVG_Video_Classifier(features=featuer_ex2, class_nr=6, optimizer=RMSprop(lr=0.0001))
     # avg_video_classifier.summary()
-
 
     # ####
     # # How to save
@@ -129,7 +140,7 @@ if __name__ == "__main__":
     # # Save only weights
     # lstm_video_classifier.save_weights("bullshit/")
     # lstm_video_classifier = None
-    
+
     # print("LOADING")
     # # Create model
     # inception = Imagenet(name='inception')
